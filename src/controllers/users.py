871376da2553697086d01e27042d58dfda2ef791
Torham <TorhamDev@gmail.com>
2024-03-10 14:56:00 +0330

    Fix register logic and add exception

--- src/controllers/users.py
+++ src/controllers/users.py
@@ -1,14 +1,15 @@
 from redis import Redis
+from src import exceptions
 
 class UserController:
     def __init__(self, redis_db: Redis) -> None:
         self.redis_db = redis_db
     
     def register(self, username: str, password: str):
-        if not self.redis_db.hgetall(f"user:{username}"):
+        if self.redis_db.hgetall(f"user:{username}"):
+            raise exceptions.UserAlreadyExists
+        else:
             self.redis_db.hset(f"user:{username}", mapping={
                 "username": username,
                 "password": password
             })
-        else:
-            raise ... # TODO

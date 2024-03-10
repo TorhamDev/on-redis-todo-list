f81ce6ab6e58be8862ebc7b3f6f67e01c33a19da
Torham <TorhamDev@gmail.com>
2024-03-10 14:55:46 +0330

    Add exceptions

---
+++ src/exceptions.py
@@ -0,0 +1,16 @@
+from fastapi.exceptions import HTTPException
+from fastapi import status
+
+
+class BaseException(HTTPException):
+    status_code = 400
+    detail = "Bad request"
+
+    def __init__(self) -> None:
+        self.status_code = self.status_code
+        self.detail = self.detail
+
+
+class UserAlreadyExists(BaseException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    detail = "user already exists."

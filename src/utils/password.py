9771b4f7df645a64782f138a91b785b47641fa4a
Torham <TorhamDev@gmail.com>
2024-03-10 15:02:43 +0330

    Add password handler using bcypt

---
+++ src/utils/password.py
@@ -0,0 +1,10 @@
+import bcrypt
+
+class PasswordHandler:
+    def hash_pass(password: str) -> str:
+        salt = bcrypt.gensalt()
+        password = bcrypt.hashpw(password.encode(), salt)
+        return password
+    
+    def verify_passowrd(hashed_pass: str, raw_pass: str):
+        return bcrypt.checkpw(raw_pass, hashed_pass)

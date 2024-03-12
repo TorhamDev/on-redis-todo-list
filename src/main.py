e568dbb43be844f69e809311ff1ede4a79ef3a7d
Torham <TorhamDev@gmail.com>
2024-03-12 15:55:00 +0330

    Add todo API router

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from src.routers.users import router as user_router
 from src.routers.login import router as login_router
+from src.routers.todo import router as todo_router
 
 app = FastAPI()
 
@@ -12,3 +13,4 @@ def index():
 
 app.include_router(user_router, prefix="/users")
 app.include_router(login_router, prefix="/login")
+app.include_router(todo_router, prefix="/todo")
